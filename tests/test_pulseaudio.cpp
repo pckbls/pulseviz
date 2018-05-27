7d2c4c4ef481d339166d24e86b23ce76053ad434
Patrick Bartels <pckbls@gmail.com>
2018-05-27 20:47:25 +0200

    Add some basic integration tests for PulseAudio class

---
+++ tests/test_pulseaudio.cpp
@@ -0,0 +1,27 @@
+#include "gtest/gtest.h"
+#include "../src/pulseaudio.h"
+
+namespace
+{
+
+TEST(PulseAudio, readSamplesPositive)
+{
+    SimpleRecordClient src(10 * 1000, "foo", "bar");
+
+    ASSERT_NO_THROW(src.getSampleRate());
+    ASSERT_NO_THROW(src.getLatency());
+
+    std::vector<float> data(512);
+    ASSERT_NO_THROW(src.read(data));
+
+    ASSERT_NO_THROW(src.flush());
+}
+
+TEST(PulseAudio, readSamplesNegative)
+{
+    SimpleRecordClient src(10 * 1000, "foo", "bar");
+    std::vector<float> data;
+    ASSERT_THROW(src.read(data), std::runtime_error);
+}
+
+} // namespace
