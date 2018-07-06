bdbd3f8a211946dcc1adf14872abdfdaa207e6bd
Patrick Bartels <pckbls@gmail.com>
2018-07-06 09:15:33 +0200

    Revert "Rewrite linspace() method"
    This reverts commit 9ccdcefdb785938abbb88180d330f9521f5953d8.

--- src/util.h
+++ src/util.h
@@ -11,13 +11,19 @@ std::string dirname(const std::string& path);
 std::string getHomeDirectory();
 bool dir_exists(const std::string& path);
 
+// See: https://stackoverflow.com/a/11747023
 template<typename T>
-std::vector<T> linspace(T start, T stop, std::size_t n)
+std::vector<T> linspace(T start, T stop, unsigned int n)
 {
-    std::vector<T> result(n);
+    std::vector<T> result;
     float step = (stop - start) / (n - 1);
-    for (unsigned int i = 0; i < result.size(); i++)
-        result[i] = static_cast<float>(i) * step;
+
+    while (start <= stop)
+    {
+        result.push_back(start);
+        start += step;
+    }
+
     return result;
 }
 
