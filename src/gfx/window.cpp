73e8c9e22fcea81250113fa008715302462646d8
Patrick Bartels <pckbls@gmail.com>
2018-07-22 11:04:32 +0200

    Enable OpenGL debug prints

--- src/gfx/window.cpp
+++ src/gfx/window.cpp
@@ -2,6 +2,29 @@
 #include <GL/glew.h>
 #include "window.h"
 
+// Taken from: https://www.khronos.org/opengl/wiki/OpenGL_Error#Catching_errors_.28the_easy_way.29
+void GLAPIENTRY
+MessageCallback(GLenum source,
+                GLenum type,
+                GLuint id,
+                GLenum severity,
+                GLsizei length,
+                const GLchar* message,
+                const void* userParam)
+{
+    (void)source;
+    (void)id;
+    (void)length;
+    (void)userParam;
+
+    fprintf(
+        stderr,
+        "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+        (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""),
+        type, severity, message
+    );
+}
+
 Window::Window()
     :
     window_handle(nullptr)
@@ -21,6 +44,11 @@ Window::Window()
         throw "Cannot initialize GLEW";
     }
     std::cout << "OpenGL version supported by this platform: " << glGetString(GL_VERSION) << std::endl;
+
+#ifndef NDEBUG
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(MessageCallback, nullptr);
+#endif
 }
 
 Window::~Window()
